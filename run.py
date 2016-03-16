47655671eb7e018cc442860eef4b4e8ff92c6576
Marko Kekäläinen (macbook) <marko.kekalainen@iki.fi>
2016-03-16 16:06:16 +0200

    Cleanup.

--- run.py
+++ run.py
@@ -2,6 +2,4 @@
 from app import app, socketio, controller
 from flask_socketio import SocketIO, send, emit
 
-#ctrl = controller.Controller()
-#ctrl.run_monitor_routines()
 socketio.run(app, debug=True)
